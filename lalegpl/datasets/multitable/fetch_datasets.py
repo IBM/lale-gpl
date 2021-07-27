eb279b60d86166b172b2507148dc170d8e803485
Chirag Sahni <sahni_chirag@yahoo.in>
2021-07-27 11:04:04 -0400

    Modified code to read the IMDB dataset in a better way (#10)
    * Modified code to read the IMDB dataset in a better way
    
    * Minor change
    
    Co-authored-by: Chirag Sahni <chirag@Chirags-MacBook-Pro.local>

--- lalegpl/datasets/multitable/fetch_datasets.py
+++ lalegpl/datasets/multitable/fetch_datasets.py
@@ -49,7 +49,7 @@ def get_data_from_csv(datatype, data_file_name):
     elif datatype.casefold() == "spark":
         if spark_installed:
             spark = SparkSession.builder.appName("GetDataset").getOrCreate()
-            return spark.read.csv(data_file_name, header=True)
+            return spark.read.options(inferSchema="True", delimiter=",").csv(data_file_name, header=True)
         else:
             raise ValueError("Spark is not installed on this machine.")
     else:
